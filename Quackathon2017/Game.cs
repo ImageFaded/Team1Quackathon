e44b71fb11e27b1f32a847c054614f15bc43a901
ImageFaded <TBWalton@dundee.ac.uk>
2017-11-11 14:22:50 +0000

    Updated name of program

--- Quackathon2017/Game.cs
+++ Quackathon2017/Game.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Quackathon2017
 {
-    public partial class Form1 : Form
+    public partial class Game : Form
     {
-        public Form1()
+        public Game()
         {
             //Test
             InitializeComponent();
