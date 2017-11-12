e57fe96c6fb310eee830131f7d61aee4a63be579
Jenimothy <dominicjonestett@labpc013>
2017-11-12 04:13:00 +0000

    The entire game basicly

--- Quackathon2017/Game.Designer.cs
+++ Quackathon2017/Game.Designer.cs
@@ -29,59 +29,90 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Game));
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.foeBox = new System.Windows.Forms.PictureBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.button1 = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.foeHb = new System.Windows.Forms.Button();
+            this.playHb = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.foeBox)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
-            // pictureBox1
+            // foeBox
             // 
-            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pictureBox1.Image = global::Quackathon2017.Properties.Resources.peopleVulture1;
-            this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
-            this.pictureBox1.Location = new System.Drawing.Point(159, 23);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(305, 234);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
-            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.foeBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.foeBox.Image = global::Quackathon2017.Properties.Resources.peopleVulture1;
+            this.foeBox.InitialImage = ((System.Drawing.Image)(resources.GetObject("foeBox.InitialImage")));
+            this.foeBox.Location = new System.Drawing.Point(188, 2);
+            this.foeBox.Name = "foeBox";
+            this.foeBox.Size = new System.Drawing.Size(305, 234);
+            this.foeBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.foeBox.TabIndex = 0;
+            this.foeBox.TabStop = false;
+            this.foeBox.Visible = false;
             // 
             // pictureBox2
             // 
             this.pictureBox2.Image = global::Quackathon2017.Properties.Resources.theGizardWizard;
             this.pictureBox2.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.InitialImage")));
-            this.pictureBox2.Location = new System.Drawing.Point(33, 280);
+            this.pictureBox2.Location = new System.Drawing.Point(0, 305);
             this.pictureBox2.Name = "pictureBox2";
             this.pictureBox2.Size = new System.Drawing.Size(216, 168);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox2.TabIndex = 1;
             this.pictureBox2.TabStop = false;
-            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            this.pictureBox2.Visible = false;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(394, 374);
+            this.button1.Location = new System.Drawing.Point(386, 305);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(98, 168);
             this.button1.TabIndex = 2;
             this.button1.Text = "Test Combat";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // foeHb
+            // 
+            this.foeHb.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.foeHb.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.foeHb.Enabled = false;
+            this.foeHb.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.foeHb.Location = new System.Drawing.Point(13, 22);
+            this.foeHb.Name = "foeHb";
+            this.foeHb.Size = new System.Drawing.Size(160, 23);
+            this.foeHb.TabIndex = 3;
+            this.foeHb.UseVisualStyleBackColor = false;
+            this.foeHb.Visible = false;
+            // 
+            // playHb
+            // 
+            this.playHb.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.playHb.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.playHb.Enabled = false;
+            this.playHb.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.playHb.Location = new System.Drawing.Point(13, 276);
+            this.playHb.Name = "playHb";
+            this.playHb.Size = new System.Drawing.Size(160, 23);
+            this.playHb.TabIndex = 4;
+            this.playHb.UseVisualStyleBackColor = false;
+            this.playHb.Visible = false;
             // 
             // Game
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(496, 473);
+            this.Controls.Add(this.playHb);
+            this.Controls.Add(this.foeHb);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.pictureBox2);
-            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.foeBox);
             this.Name = "Game";
             this.Text = "Game";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.Load += new System.EventHandler(this.Game_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.foeBox)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
 
@@ -89,9 +120,11 @@
 
         #endregion
 
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox foeBox;
         private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button foeHb;
+        private System.Windows.Forms.Button playHb;
     }
 }
 
--- Quackathon2017/Game.cs
+++ Quackathon2017/Game.cs
@@ -14,10 +14,27 @@ namespace Quackathon2017
     //Set up classes
     public partial class Game : Form
     {
-        int choice;
+        Random randon = new Random();
 
+        //int choice;
+        Opponent foeHolder;
+        Player playeer = new Player();
         SpeechRecognitionEngine recEngine = new SpeechRecognitionEngine();
         bool gameState = true;
+
+        private void Game_Load(object sender, EventArgs e)
+        {
+            Choices cmds = new Choices();
+            cmds.Add(new string[] { "say hello", "regret existing", "fire", "nuclear", "Lightning", "heal", "consider suicide"});
+            GrammarBuilder gBuild = new GrammarBuilder();
+            gBuild.Append(cmds);
+            Grammar gram = new Grammar(gBuild);
+
+            recEngine.LoadGrammarAsync(gram);
+            recEngine.SetInputToDefaultAudioDevice();
+            recEngine.SpeechRecognized += reco;
+        }
+
         public Game()
         {
             MaximizeBox = false;
@@ -27,125 +44,236 @@ namespace Quackathon2017
             InitializeComponent();
         }
 
-        protected override void OnLoad(EventArgs e)
-        {
+        #region overworld
 
-            Choices commands = new Choices();                                           //the choices object contains the words that the Speech recognistion software will be interpreting
-            commands.Add(new String[] { "fire" });      //adds "say hello" and "print my name" as commands the computer will understand
-            GrammarBuilder gb = new GrammarBuilder();                                   //Grammerbuilder object is used to pass the commands to the Grammer object
-            gb.Append(commands);                                                        //add commands to gb object
-            Grammar grammer = new Grammar(gb);                                          // Grammer object is passed to the speech recognition engine 
-
-
-            recEngine.LoadGrammarAsync(grammer);                            //tells the speech recognition engine what to scan for while the engine is running
-            try                                                             //stops an error ocurring if no microphone is connected
-            {
-                recEngine.SetInputToDefaultAudioDevice();                   //use the current default audio input device
-                                                                            //attempt to recognise user speech
-
-            }
-            catch (Exception)
-            {
-                MessageBox.Show("No Microphone Detected!");                 //tell the user if no microphone is connected
-            }
-
-
-        }
-
-        public void controlCharacter(object sender, SpeechRecognizedEventArgs e)
-        {
-            ///Danial black lee les
-            ///Block lesss
-            //Maybe more
-            while (true)
-            {
-                switch (e.Result.Text)
-                {
-                    case "Fire":
-                        //I am wizard, i am going to set this on fire
-                        //Damage enemy
-                        choice = 1;
-                        break;
-                    case "backwards":
-                        break;
-                    case "left":
-                        break;
-                    case "right":
-                        break;
-                }
-            }
-        }
-        public int Combat(PeopleVulture foe, Player player, int conditions)
-        {
-            //Clear screen, show combat icons
-            //Show starting effects from conditions
-            //Do player action
-            playerAction(foe);
-            //Do enemy action
-            //Enemy action
-
-
-            //Others?
-
-            return 0;
-        }
-
-        public void playerAction(PeopleVulture foe)
-        {
-            recEngine.SpeechRecognized += controlCharacter;
-            switch (choice)
-            {
-                case 1:
-                    bool vic = foe.HealthVary(4);
-                    MessageBox.Show("DAMAGE DONE");
-                    break;
-                case 2:
-                    break;
-                case 3:
-                    break;
-                case 4:
-                    break;
-            }
-            if (choice == 1)
-            {
-                //Do damage
-
-
-            }
-        }
-        public void foeAction()
-        {
-            //Do option
-        }
-
-        private void pictureBox2_Click(object sender, EventArgs e)
+        public void setUpGrid()
         {
 
         }
 
-        private void pictureBox1_Click(object sender, EventArgs e)
-        {
+#endregion
 
-        }
+
+
+
 
         private void button1_Click(object sender, EventArgs e)
         {
+
             //Instatiate player object
             //Instatiate people vulture
-            PeopleVulture nonagon = new PeopleVulture();
-            nonagon.SetStats(5, 1, 3, 0, 2);
+            Opponent nonagon = getRanFoe();
+            nonagon.setUp();
             nonagon.displayStats();
-            Player playeer = new Player();
-            Combat(nonagon, playeer, 0);
+            combat(nonagon, playeer, 0);
 
             //Show em both
 
         }
+
+        public Opponent getRanFoe()
+        {
+            if (randon.Next(0, 3) == 0) {return new RoadTrain(); }
+            else { return new PeopleVulture(); }
+        }
+
+        #region combatEngine
+
+        public void combat(Opponent oposition, object player, int effects)
+        {
+            //SET UP SCREEN FOR WOMBAT
+            gameState = false;
+            pictureBox2.Visible = true;
+            foeBox.Visible = true;
+            foeHb.Visible = true;
+            playHb.Visible = true;
+            button1.Enabled = false;
+            //Done hiding objects
+            foeHolder = oposition;
+            MessageBox.Show("Test1");
+            updateHealth();
+            try
+            {
+                recEngine.RecognizeAsync(RecognizeMode.Single);
+            } catch (InvalidOperationException)
+            {
+                MessageBox.Show("Stop hitting the start button so much!");
+            }
+        }
+
+        public void updateHealth()
+        {
+            //Set health bar to ten times the health of the player
+            playHb.Width = 10 * playeer.getHp();
+            foeHb.Width = 10 * foeHolder.getHp();
+        }
+
+        public void nextTurn()
+        {
+            updateHealth();
+            recEngine.RecognizeAsync(RecognizeMode.Single);
+        }
+
+        private void reco(object sender, SpeechRecognizedEventArgs e)
+        {
+            MessageBox.Show("TEST 3");
+            Spells newSpl;
+            switch (e.Result.Text)
+            {
+                case "say hello":
+                    MessageBox.Show("GENERAL KANOBI");
+                    recEngine.RecognizeAsyncStop();
+                    break;
+                case "regret existing":
+                    MessageBox.Show("KILL YR SELF");
+                    recEngine.RecognizeAsyncStop();
+                    foeTurn();
+                    break;
+                case "fire":
+                    newSpl = new fire();
+                    recEngine.RecognizeAsyncStop();
+                    newSpl.setUp();
+                    spellEffect(newSpl);
+                    break;
+                case "nuclear":
+                    MessageBox.Show("Nuclear");
+                    //Auto win round
+                    recEngine.RecognizeAsyncStop();
+                    break;
+                case "Lightning":
+                    newSpl = new lightningBolt();
+                    recEngine.RecognizeAsyncStop();
+                    newSpl.setUp();
+                    spellEffect(newSpl);
+                    break;
+                case "heal":
+                    newSpl = new Heal();
+                    recEngine.RecognizeAsyncStop();
+                    newSpl.setUp();
+                    spellEffect(newSpl);
+                    break;
+                case "consider suicide":
+                    newSpl = new MindCtrl();
+                    recEngine.RecognizeAsyncStop();
+                    newSpl.setUp();
+                    spellEffect(newSpl);
+                    break;
+
+            }
+        }
+
+        public void spellEffect(Spells cast)
+        {
+            MessageBox.Show("CAST :"+cast.getName());
+            bool kill = false;
+            if (cast.getEffect() == 0)
+            {
+                //Damaging attac
+                kill = foeHolder.HealthVary(cast.getValue());
+            }
+            if (cast.getEffect() == 1)
+            {
+                //Boost Atac
+               //Does nothig
+            }
+            if (cast.getEffect() == 2)
+            {
+                //Bost defence
+                //does nothing
+            }
+            if (cast.getEffect() == 3)
+            {
+                //Heal
+                playeer.damage(cast.getValue() * -1);
+            }
+
+            if (kill == true)
+            {
+                endCombat();
+            }
+            else
+            {
+                foeTurn();
+            }
+        }
+
+        public void foeTurn()
+        {
+            updateHealth();
+            //D
+            bool deed = foeAttack();
+            for (int i = 0; i < 4; i++)
+            { foeHolder.moves[i].cool(); }
+            if (deed == true) { MessageBox.Show("YOU ARE DEED"); endCombat(); }
+            else {MessageBox.Show("CASE YOUR SP'EL"); nextTurn();  }
+
+        }
+
+        public bool foeAttack()
+        {
+            bool deed = false;
+            //Select attack
+            //If random attack is usable, then perfomr it, else call foeAttack Again
+            int moveChoice = randon.Next(0, 4);
+            if ((foeHolder.moves[0].checkUse() == false) && (foeHolder.moves[1].checkUse() == false) && (foeHolder.moves[2].checkUse() == false) && (foeHolder.moves[3].checkUse() == false)) { MessageBox.Show("THE PEOPLE VULTURE USED STRUGGLE"); deed = playeer.damage(1); }
+                if (foeHolder.moves[moveChoice].checkUse() == true  )
+            {
+                //WE DO THE MOOOOVE
+                //DECLARE THE MOOOOVE
+                foeHolder.moves[moveChoice].timer = foeHolder.moves[moveChoice].cooldown;
+                MessageBox.Show("THE "+ foeHolder.getName() + " USED " + foeHolder.moves[moveChoice].name);
+                //WE WORK OUT IT's EFFECT
+                if (foeHolder.moves[moveChoice].effect == 0)
+                {
+                    //GENERIC ATTACK
+                    deed = playeer.damage(foeHolder.moves[moveChoice].value);
+                }
+                if (foeHolder.moves[moveChoice].effect == 1)
+                {
+                    //INCREASE ATTACK
+                    foeHolder.attackC(foeHolder.moves[moveChoice].value);
+                }
+                if (foeHolder.moves[moveChoice].effect == 2)
+                {
+                    //INCREASE DEFENCE
+                    foeHolder.defenceC(foeHolder.moves[moveChoice].value);
+                }
+                if (foeHolder.moves[moveChoice].effect == 3)
+                {
+                    //HEAL SATAN
+                    foeHolder.HealthVary(-1*(foeHolder.moves[moveChoice].value));
+                }
+            }
+            else { MessageBox.Show("ARE WE STUCK FOREVER?"); deed = foeAttack(); }
+            return deed;
+        }
+
+
+
+        public void endCombat()
+        {
+            MessageBox.Show("COMBAT OVER");
+            pictureBox2.Visible = false;
+            foeBox.Visible = false;
+            foeHb.Visible = false;
+            playHb.Visible = false;
+            button1.Enabled = true;
+            gameState = true;
+        }
+
+        #endregion
+
+
     }
+
+    #region objects
+
+    //Objects
 
     //Defines player object
     public class Player
     {
+
         //Set health value
         int health = 10;
         //Returns health value
@@ -164,27 +292,41 @@ namespace Quackathon2017
         }
     }
 
+    #region enemy Classes
+
     public abstract class Opponent
     {
+        public Moves[] moves = new Moves[4];
         int healthPoints;
+        string name;
         int attack;
         int defense;
         int special;
-        int element;
+        string element;
         int temp;
 
-        public void SetStats(int healthPoints, int attack, int defense, int special, int element)
+        public virtual void setUp()
+        {
+
+        }
+
+        public void SetStats(int healthPoints, int attack, int defense, int special, string element, string name)
         {
             this.healthPoints = healthPoints;
             this.attack = attack;
             this.defense = defense;
             this.special = special;
             this.element = element;
+            this.name = name;
         }
 
         public void displayStats()
         {
-            MessageBox.Show("Health : " + healthPoints + " Attack  : " + attack + " Defense : " + defense + " Special : " + special + " Element : " + element);
+            MessageBox.Show("Health : " + healthPoints + " Attack  : " + attack + " Defense : " + defense + " Special : " + special + " Element : " + element + " Name : " + name);
+        }
+        public string getName()
+        {
+            return name;
         }
 
         public int Attack()
@@ -210,15 +352,217 @@ namespace Quackathon2017
 
             }
         }
-
-      
+        public void attackC(int change)
+        {
+            attack = attack + change;
+        }
+        public void defenceC(int change)
+        {
+            defense = defense + change;
+        }
+        public int getHp()
+        {
+            return healthPoints;
+        }
     }
+
+
 
     public class PeopleVulture : Opponent
     {
 
+        public override void setUp()
+        {
+            moves[0] = new cry(); moves[1] = new generic(); moves[2] = new generic(); moves[3] = new cry();
+            //Set my stats
+
+            SetStats(5, 1, 3, 0, "vulture", "People Vulture");
+        }
+    }
+    public class RoadTrain : Opponent
+    {
+        public override void setUp()
+        {
+            moves[0] = new Plough(); moves[1] = new FullSteamAhead(); moves[2] = new Horn(); moves[3] = new generic();
+
+            SetStats(8, 7, 2, 0, "robot", "ROAD TRAIN");
+        }
     }
 
+    #endregion
 
-    
+    #region move classes
+
+
+    public abstract class Moves
+    {
+        public bool active; //If a move is an attack or a passive effect
+        public string name; //The name of the move
+        public int effect; //The effect of the move 0. Normal Attack, 1. Buff Attack, 2. Buff Defence, 3. Heal, 4. Attack (Ignores defence)
+        public string element; //The element of the move Lightning, Fire, Robot, Vulture, Beast Etc Etc
+        public int value; //The numerical value of the move
+        public int cooldown; //The cooldown time of the move
+        public int timer; //How long the move has left
+        public void cool()
+        {
+            if (timer > 0)
+            {
+                timer--;
+            }
+        }
+        public bool checkUse()
+        {
+            if (timer > 0) { return false; }
+            else { return true; }
+        }
+
+    }
+
+    public class cry : Moves
+    {
+        public cry()
+        {
+            active = true;
+            name = "Cry";
+            effect = 1;
+            element = "Vulture";
+            value = 2;
+            cooldown = 2;
+        }
+    }
+    public class generic : Moves
+    {
+        public generic()
+        {
+            active = true;
+            name = "Generic";
+            effect = 0;
+            element = "Vulture";
+            value = 4;
+            cooldown = 1;
+        }
+    }
+
+    public class Plough : Moves
+    {
+        public Plough()
+        {
+            active = true;
+            name = "Plough";
+            effect = 0;
+            element = "Robot";
+            value = 3;
+            cooldown = 2;
+        }
+    }
+
+    public class FullSteamAhead : Moves
+    {
+        public FullSteamAhead()
+        {
+            active = true;
+            name = "Full Steam Ahead";
+            effect = 0;
+            element = "Robot";
+            value = 7;
+            cooldown = 5;
+        }
+    }
+
+    public class Horn : Moves
+    {
+        public Horn()
+        {
+            active = true;
+            name = "Horn";
+            effect = 1;
+            value = 2;
+            cooldown = 2;
+
+        }
+    }
+
+    #endregion
+
+    #region spells
+
+
+    public abstract class Spells
+    {
+        bool active; //If a move is an attack or a passive effect
+        string name; //The name of the move
+        int effect; //The effect of the move 0. Normal Attack, 1. Buff Attack, 2. Buff Defence, 3. Heal, 4. Attack (Ignores defence)
+        string element; //The element of the move Lightning, Fire, Robot, Vulture, Beast Etc Etc
+        int value; //The numerical value of the move
+        public virtual void setUp()
+        {
+
+        }
+        public string getName()
+        {
+            MessageBox.Show("AM I BEING CALLED?");
+
+            return name;
+        }
+        public int getEffect()
+        {
+            return effect;
+        }
+        public string getElement()
+        {
+            return element;
+        }
+        public int getValue()
+        {
+            return value;
+        }
+        public void setVals(string name, int effect, string element, int value)
+        {
+            this.name = name; this.effect = effect; this.element = element; this.value = value;
+        }
+
+    }
+
+    public class fire : Spells
+    {
+        public override void setUp()
+        {
+            setVals("FIREBALL", 0, "FIRE", 5);
+        }
+    }
+    public class lightningBolt : Spells
+    {
+        public override void setUp()
+        {
+            setVals("Lightning Bolt", 0, "Lightning", 7);
+        }
+    }
+    public class Heal : Spells
+    {
+        public override void setUp()
+        {
+            setVals("HEAL", 3, "N/A", 3);
+        }
+    }
+
+    public class MindCtrl : Spells
+    {
+        public override void setUp()
+        {
+            setVals("Mind Cntrl", 0, "VULTURE", 20);
+        }
+    }
+
+    #endregion
+
+    #endregion
+
+
+
+
+
+
+
+
+
 }
